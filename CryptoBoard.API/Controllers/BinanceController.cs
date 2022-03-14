1f045b86efa252c84b6080ec560256992e831906
Tiago <Jurandir@DESKTOP-QSCPMAU>
2022-03-14 10:00:01 -0300

    integracao binance

---
+++ CryptoBoard.API/Controllers/BinanceController.cs
@@ -0,0 +1,25 @@
+﻿using CryptoBoard.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+
+namespace CryptoBoard.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class BinanceController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetSymbols()
+        {
+            var url = "https://api.binance.com/api/v3/exchangeInfo";
+
+            var result = await url.GetJsonAsync<CoinsList>();
+
+            return Ok(result);
+        }
+    }
+}
